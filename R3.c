ef01c5fe5f760a0f0309dff54d711327f866e6db
Kitzia <112416148+KitziaAra@users.noreply.github.com>
2024-11-27 13:30:55 -0600

    Update R3.c
    Pito con mole

--- R3.c
+++ R3.c
@@ -15,9 +15,12 @@ int sel(int **, int ***, int ***, int *);
 int sumArr(int *, int, int);
 void maxSum(int *, int, int, int *, int *, int *);
 void maxSumMD(int *, int, int *, int *, int *);
+void imprimeMatriz(int **m, int n);
+void prodMatrizRec(int **, int **, int ***, int, int , int, int);
+void prodMatriz(int ***, int ***, int n);
 
 int main(){
-    int n = 0, i = 0, mayor = 0, ini = 0, fin = 0, *a, **m1, **m2, s;
+    int n = 0, i = 0, mayor = 0, ini = 0, fin = 0, *a, **m1, **m2, **c, s;
     s = sel(&a, &m1, &m2, &n);
 
     if(s == 1){
@@ -37,6 +40,24 @@ int main(){
     }else{
         puts("========================================================");
         puts("===============Multiplicacion de matrices===============");
+	
+	    
+	//Creando c (la matriz suma) e inicianizandola en 0
+	c = (int **) malloc(n*sizeof(int *));
+	for(int x=0; x<n; x++){
+		c[x]= (int *) malloc(n*sizeof(int));
+		for(int y=0; y<n; y++)
+			c[x][y] = 0;
+	}
+	putchar('\n');
+        printf("Producto de matrices iterativo: \n");
+        prodMatriz(&m1,&m2, n);
+        putchar('\n');
+
+        
+	printf("Producto recursivo: \n");
+	prodMatrizRec(m1, m2, &c, n, 0, 0, 0);
+	imprimeMatriz(c, n);
     }
 
     return 0;
@@ -59,7 +80,37 @@ int sel(int **a, int ***m1, int ***m2, int *n){
             scanf("%d", (*a)+i);
         }
     }else if(s == 2){
+	    printf("Ingrese la dimension de las matrices cuadradas: \n");
+	    scanf("%d", &n);
 
+	    //Se crea la matriz 1
+	    (*m1) = (int **) malloc(n*sizeof(int *)); 
+	    for(int x=0; x<n; x++){
+		    (*m1)[x]= (int *) malloc(n*sizeof(int));
+	    }
+	    
+	    //Creando la matriz 2
+	    (*m2) = (int **) malloc(n*sizeof(int *));
+	    for(int x=0; x<n; x++){
+		    (*m2)[x]= (int *) malloc(n*sizeof(int));
+	    }
+	    
+	    //pidiendo la informacion de la matriz 1
+	    printf("Ingrese los datos de la matriz 1: \n");
+	    for(int i=0; i<n; i++){
+		    for(int j=0; j<n; j++){
+			    printf("m1[%d][%d]= ", i, j);
+			    scanf("%d", m1[i][j]);
+		    }
+	    }
+	    //Pidiendo al ususario los datos de la matriz 2
+	    printf("Ingrese los datos de la matriz 2: \n");
+	    for(int i=0; i<n; i++){
+		    for(int j=0; j<n; j++){
+			    printf("m2[%d][%d]= ", i, j);
+			    scanf("%d", m2[i][j]);
+		    }
+	    }
     }else{
         puts("Opcion no valida");
         return 0;
@@ -119,7 +170,59 @@ int sumArr(int *arr, int m, int n){
     }
     return sum;
 }
+/*Una función que imprime una matriz almacenada en un doble apuntador*/
+void imprimeMatriz(int **m, int n){
+    for(int i=0; i<n;i++){
+        for(int j=0; j<n; j++){
+            printf("\t%d", m[i][j]);
+        }
+        printf("\n");
+    }
 
+}
+
+/*la funcion que calcula el producto de dos matrices mediante divide y vencerás y almacena en el arreglo c proporcionado*/
+void prodMatrizRec(int **m1, int **m2, int ***c, int n, int i, int j, int k){
+    //caso base
+     if(i>=n){
+        return;
+    }
+
+    if(j>=n){
+        prodMatrizRec(m1, m2, c, n, i+1, 0, 0);
+        return;
+    }
+
+    if(k<n){
+	//Se suma el producto de los dos terminos actuales
+        ((*c)[i][j]) += ((m1[i][k])*(m2[k][j]));
+        prodMatrizRec(m1, m2, c, n, i, j, k+1);
+    }else{
+        prodMatrizRec(m1, m2, c, n, i, j+1, 0);
+    }
+}
+
+/*Una funcion que calcula la multiplicación de dos matrices mediante 3 fors anidados iterativamente*/
+void prodMatriz(int ***m1, int ***m2, int n){
+    int i,j,k;
+
+    int **c = (int **) malloc(n*sizeof(int *));
+    for(int x=0; x<n; x++){
+        c[x]= (int *) malloc(n*sizeof(int));
+        for(int y=0; y<n; y++)
+            c[x][y] = 0;
+    }
+
+    for(i=0; i<n; i++){
+        for(j=0; j<n; j++){
+            for(k=0; k<n; k++){
+                c[i][j] += (* m1)[i][k]*(* m2)[k][j];
+            }
+        }
+    }
+
+    imprimeMatriz(c, n);
+}
 /*Esta funcion de conteo de inversiones es utilizando el metodo directo, en esta se utilizan dos fors anidados
 el primer for controlara de izquierda a derecha el primer elemento que se use para una comparacion para saber
 si se aumenta las inversiones, el segundo for manejara el segundo elemento para la misma comparacion
